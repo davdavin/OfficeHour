7045ace07d0e1fc6a93ce943b5310a83e4577e2d
happypeoplee <raidavr7@gmail.com>
2022-02-08 19:52:55 +0700

    update

--- application/models/M_Admin.php
+++ application/models/M_Admin.php
@@ -6,6 +6,15 @@ class M_Admin extends CI_Model
         return $this->db->get_where($table, $where);
     }
 
+    function tampil_subscriber()
+    {
+        $this->db->select('id_perusahaan, nama_perusahaan, nama_paket, harga, tanggal_bayar, status_perusahaan');
+        $this->db->from('perusahaan');
+        $this->db->join('subscribe', 'perusahaan.id_perusahaan = subscribe.id_perusahaan');
+        $this->db->join('paket', 'subscribe.id_paket = paket.id_paket');
+        return $this->db->get();
+    }
+
     function tampil_paket()
     {
         return $this->db->query("SELECT * FROM paket");
