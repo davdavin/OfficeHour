a2186e3c5c5e0c228c834e4d97d04155b691eadb
happypeoplee <raidavr7@gmail.com>
2022-02-09 19:25:33 +0700

    update

--- application/views/message.php
+++ application/views/message.php
@@ -4,6 +4,12 @@
 }
 unset($_SESSION['sukses']); ?>
 
+<?php if ($this->session->has_userdata('info')) { ?>
+    <div class="info" data-flashdata="<?php echo $this->session->flashdata('info'); ?>"></div>
+<?php
+}
+unset($_SESSION['info']); ?>
+
 <?php if ($this->session->has_userdata('gagal')) { ?>
     <div class="gagal" data-flashdata="<?php echo $this->session->flashdata('gagal'); ?>"></div>
 <?php
