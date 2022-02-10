7a8d8a8cf8de1567f39c779ab3e78b11247a69c6
happypeoplee <raidavr7@gmail.com>
2022-02-10 21:24:24 +0700

    tambah function

--- application/controllers/TimeTracker.php
+++ application/controllers/TimeTracker.php
@@ -10,10 +10,14 @@ class TimeTracker extends CI_Controller
         if ($this->session->userdata('status_login_karyawan') != "login") {
             redirect('Login/login_karyawan');
         }
+
+        $this->load->model(array('M_Karyawan'));
     }
 
     function index()
     {
+        $this->session->userdata('nama_karyawan');
+        // $data['aktivitas']
         $this->load->view('v_timetracker.php');
     }
 }
