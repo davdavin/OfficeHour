01df946e799fb6468759509b5656d46cf6d6fec6
happypeoplee <raidavr7@gmail.com>
2022-02-13 20:06:04 +0700

    update

--- application/views/v_lihat_daftar_klien.php
+++ application/views/v_lihat_daftar_klien.php
@@ -147,7 +147,7 @@
                 <div class="container-fluid">
                     <div class="card">
                         <div class="card-header">
-                            <h3 class="card-title">Daftar Karyawan</h3>
+                            <h3 class="card-title">Daftar Klien</h3>
                         </div>
 
                         <div class="card-body">
