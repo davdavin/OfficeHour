f8bed35d63172db98891f4d8b3fe0a291db24d2c
happypeoplee <raidavr7@gmail.com>
2022-03-08 23:07:45 +0700

    function baru

--- application/models/M_Project.php
+++ application/models/M_Project.php
@@ -1,6 +1,12 @@
 <?php
 class M_Project extends CI_Model
 {
+    function tampil_anggota($id_project)
+    {
+        return $this->db->query("SELECT id_anggota_project, nama_karyawan FROM anggota_project JOIN karyawan 
+                                ON anggota_project.id_karyawan = karyawan.id_karyawan WHERE id_project = '$id_project'");
+    }
+
     function insert_record($data, $table)
     {
         $this->db->insert($table, $data);
