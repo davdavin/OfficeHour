2c17048630e3a1200e3a1ad154d8b4dce8d2d91f
happypeoplee <raidavr7@gmail.com>
2022-03-12 23:27:55 +0700

    tambah function baru

--- application/models/M_Project.php
+++ application/models/M_Project.php
@@ -19,6 +19,11 @@ class M_Project extends CI_Model
                                 INNER JOIN tugas_project ON tugas_project.id_anggota_project = anggota_project.id_anggota_project WHERE id_perusahaan = '$id_perusahaan' ");
     }
 
+    function project_detail($id_project, $id_perusahaan)
+    {
+        return $this->db->query("SELECT * FROM project JOIN karyawan ON karyawan.id_karyawan = project.project_manager WHERE id_project = '$id_project' AND project.id_perusahaan = '$id_perusahaan'");
+    }
+
     function insert_record($data, $table)
     {
         $this->db->insert($table, $data);
