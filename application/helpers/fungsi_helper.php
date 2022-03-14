8f59c493a5ef2802ce03853e21006ebdbd0d2c2b
happypeoplee <raidavr7@gmail.com>
2022-03-14 21:20:57 +0700

    function baru untuk format waktu

--- application/helpers/fungsi_helper.php
+++ application/helpers/fungsi_helper.php
@@ -1,12 +1,20 @@
 <?php
 function tanggal_indonesia($tanggal_lengkap)
 {
+    $tanggal = date_format(date_create($tanggal_lengkap), "d/n/Y");
+
     $nama_bulan = array(
         1 => 'Januari', 2 => 'Februari', 3 => 'Maret', 4 => 'April', 5 => 'Mei', 6 => 'Juni', 7 => 'Juli', 8 => 'Agustus',
         9 => 'September', 10 => 'Oktober', 11 => 'November', 12 => 'Desember'
     );
-    $pisah = explode("/", $tanggal_lengkap);
+    $pisah = explode("/", $tanggal);
 
     $tanggal_baru = $pisah[0] . ' ' . $nama_bulan[$pisah[1]] . ' ' . $pisah[2];
     return $tanggal_baru;
+}
+
+function waktu($waktu)
+{
+    $waktu_baru = date_format(date_create($waktu), "H:i");
+    return $waktu_baru;
 }
