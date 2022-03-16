1c23defbf56e5dd10b19f4e8f3a3d6a8c3ebe6f5
happypeoplee <raidavr7@gmail.com>
2022-03-16 17:34:56 +0700

    edit kata klien

--- application/views/v_login_klien.php
+++ application/views/v_login_klien.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login - Cient</title>
+    <title>Login - Klien</title>
 
     <!-- Favicons -->
     <link href="<?php echo base_url(); ?>assets/dist/img/logo.png" rel="icon">
