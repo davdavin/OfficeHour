dbfe54195a4f5f885020104e04b638013f54d62e
happypeoplee <raidavr7@gmail.com>
2022-03-16 21:10:14 +0700

    halaman baru untuk menampilkan project dari klien

---
+++ application/views/v_detail_project_klien.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>OfficeHour - Karyawan</title>
+
+    <!-- Favicons -->
+    <link href="<?php echo base_url(); ?>assets/dist/img/logo.png" rel="icon">
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/fontawesome-free/css/all.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- JQVMap -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/jqvmap/jqvmap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/adminlte.min.css">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/daterangepicker/daterangepicker.css">
+    <!-- summernote -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/summernote/summernote-bs4.min.css">
+    <!-- SweetAlert2 -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+
+        <!-- Preloader -->
+        <div class="preloader flex-column justify-content-center align-items-center">
+            <img class="animation__shake" src="<?php echo base_url(); ?>assets/dist/img/logo.png" alt="Logo" height="60" width="60">
+        </div>
+
+        <!-- Navbar -->
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <!-- Left navbar links -->
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                </li>
+                <li class="nav-item d-none d-sm-inline-block">
+                    <a href="" class="nav-link">Home</a>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <!-- Brand Logo -->
+            <a class="brand-link">
+                <img src="<?php echo base_url(); ?>assets/dist/img/logo.png" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <span class="brand-text font-weight-light">OfficeHour</span>
+            </a>
+
+            <!-- Sidebar -->
+            <div class="sidebar">
+                <!-- Sidebar user panel (optional) -->
+                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                    <div class="image">
+                        <img src="<?php echo base_url(); ?>assets/dist/img/avatar5.png" class="img-circle elevation-2" alt="User Image">
+                    </div>
+                    <div class="info">
+                        <a href="#" class="d-block"><?php echo $this->session->userdata('nama_klien') ?></a>
+                    </div>
+                </div>
+
+                <!-- Sidebar Menu -->
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                        <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+                        <li class="nav-item menu-open">
+                            <a href="<?php echo base_url() . 'Klien' ?>" class="nav-link">
+                                <i class="nav-icon fas fa-clock"></i>
+                                <p> Dashboard </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="<?php echo base_url() . 'Account_Klien/profile/' . $this->session->userdata('id_klien')  ?>" class="nav-link">
+                                <i class="nav-icon fas fa-calendar"></i>
+                                <p> Akun </p>
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="<?php echo base_url() . 'Login/logout_klien' ?>" class="nav-link">
+                                <i class="nav-icon fas fa-power-off"></i>
+                                <p> Keluar </p>
+                            </a>
+                        </li>
+
+
+                    </ul>
+                </nav>
+                <!-- /.sidebar-menu -->
+            </div>
+
+            <!-- /.sidebar -->
+        </aside>
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>Informasi Project</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active">Dashboard</li>
+                                <li class="breadcrumb-item active">Project Detail</li>
+                            </ol>
+                        </div><!-- /.col -->
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <!-- Small boxes (Stat box) -->
+                    <div class="row">
+                        <section class="col-lg-8">
+                            <div class="card card-primary">
+                                <div class="card-body">
+                                    <div class="text-center">
+                                        <h4>Project Detail</h4>
+                                    </div>
+                                    <?php foreach ($project_detail as $list) { ?>
+
+                                        <table class="table">
+                                            <tr>
+                                                <th>Nama Project</th>
+                                                <td><?php echo $list->nama_project; ?></td>
+                                            </tr>
+                                            <tr>
+                                                <th>Project Manager</th>
+                                                <td><?php echo $list->nama_karyawan; ?></td>
+                                            </tr>
+                                            <tr>
+                                                <th>Tanggal Mulai</th>
+                                                <td><?php echo tanggal_indonesia($list->tanggal_mulai_project); ?></td>
+                                            </tr>
+                                            <tr>
+                                                <th>Tanggal Selesai</th>
+                                                <td><?php echo tanggal_indonesia($list->tanggal_selesai_project); ?></td>
+                                            </tr>
+                                            <tr>
+                                                <th>Status</th>
+                                                <td><?php echo ucwords(strtolower($list->status_project)); ?></td>
+                                            </tr>
+                                            <tr>
+                                                <th>Deskripsi</th>
+                                                <td><?php echo $list->deskripsi_project; ?></td>
+                                            </tr>
+                                        </table>
+                                    <?php } ?>
+                                </div>
+                            </div>
+                            <div class="card card-primary">
+                                <div class="card-body">
+                                    <div class="text-center">
+                                        <h4>Anggota Project</h4>
+                                    </div>
+                                    <table id="list-anggota" class="table table-bordered table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th>Nama Karyawan</th>
+                                                <th>Nama Tugas</th>
+                                                <th>Batas Waktu</th>
+                                                <th>Status Tugas</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php foreach ($anggota_project as $detail) { ?>
+                                                <tr>
+                                                    <td><?php echo $detail->nama_karyawan ?></td>
+                                                    <td><?php echo $detail->nama_tugas ?></td>
+                                                    <td><?php echo tanggal_indonesia($detail->batas_waktu) ?></td>
+                                                    <td><?php echo $detail->status_tugas ?></td>
+                                                </tr>
+                                            <?php } ?>
+                                        </tbody>
+
+                                    </table>
+                                </div>
+                            </div>
+                        </section>
+                        <section class="col-lg-4">
+                            <!-- small box -->
+                            <div class="small-box col-7 bg-info" style="border-radius: 15px;">
+                                <div class="inner text-center">
+                                    <h4>Total Tugas</h4>
+                                    <h4>
+                                        <?php foreach ($total_tugas as $total) {
+                                            echo $total->totalTugas;
+                                        }
+                                        ?>
+                                    </h4>
+                                </div>
+                            </div>
+                            <?php foreach ($total_status as $totalS) {
+                                if ($totalS->status_tugas == "SEDANG BERJALAN") { ?>
+                                    <div class="small-box col-7 bg-maroon" style="border-radius: 15px;">
+                                        <div class="inner text-center">
+                                            <h4>Sedang Berjalan</h4>
+                                            <h4>
+                                                <?php echo $totalS->totalStatus;
+                                                ?>
+                                            </h4>
+                                        </div>
+                                    </div>
+                            <?php }
+                            } ?>
+
+                            <?php foreach ($total_status as $totalS) {
+                                if ($totalS->status_tugas == "SELESAI") { ?>
+                                    <div class="small-box col-7 bg-green" style="border-radius: 15px;">
+                                        <div class="inner text-center">
+                                            <h4>Selesai</h4>
+                                            <h4> <?php echo $totalS->totalStatus; ?>
+                                            </h4>
+                                        </div>
+                                    </div><br><br>
+                            <?php }
+                            } ?>
+                        </section>
+                    </div>
+            </section>
+        </div>
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- jQuery -->
+    <script src="<?php echo base_url(); ?>assets/plugins/jquery/jquery.min.js"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="<?php echo base_url(); ?>assets/plugins/jquery-ui/jquery-ui.min.js"></script>
+    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+    <script>
+        $.widget.bridge('uibutton', $.ui.button)
+    </script>
+    <!-- Bootstrap 4 -->
+    <script src="<?php echo base_url(); ?>assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Tempusdominus Bootstrap 4 -->
+    <script src="<?php echo base_url(); ?>assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+    <!-- Summernote -->
+    <script src="<?php echo base_url(); ?>assets/plugins/summernote/summernote-bs4.min.js"></script>
+    <!-- overlayScrollbars -->
+    <script src="<?php echo base_url(); ?>assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="<?php echo base_url(); ?>assets/dist/js/adminlte.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="<?php echo base_url(); ?>assets/dist/js/demo.js"></script>
+    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+    <script src="<?php echo base_url(); ?>assets/dist/js/pages/dashboard.js"></script>
+    <!-- DataTables  & Plugins -->
+    <script src="<?php echo base_url(); ?>assets/plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/plugins/jszip/jszip.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/plugins/pdfmake/pdfmake.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/plugins/pdfmake/vfs_fonts.js"></script>
+    <script src="<?php echo base_url(); ?>assets/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+
+    <script>
+        $(function() {
+            $("#list-anggota").DataTable({
+                "responsive": true,
+                "lengthChange": true,
+                "autoWidth": false
+            });
+        });
+    </script>
+</body>
+
+
+</html>
