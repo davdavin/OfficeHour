1abf2dfc45ebe91a95f51e4f33dfc9d6d8dc3102
happypeoplee <raidavr7@gmail.com>
2022-03-30 19:09:09 +0700

    update di baris 58

--- application/controllers/TimeTracker.php
+++ application/controllers/TimeTracker.php
@@ -55,6 +55,20 @@ class TimeTracker extends CI_Controller
         $waktu_selesai = $this->input->post('waktu_selesai');
         $bukti = $this->input->post('bukti');
         $status = $this->input->post('status_tugas');
+        $time = new DateTime($waktu_mulai);
+        $time2 = new DateTime($waktu_selesai);
+        $interval = $time2->diff($time);
+        $durasi = $interval->format('%H');
+        $hari = date('D', strtotime($tanggal_aktivitas));
+
+
+        /*
+         $time = new DateTime($list_aktivitas->waktu_mulai);
+                        $time2 = new DateTime($list_aktivitas->waktu_selesai);
+                        $interval = $time2->diff($time);
+                        $durasi = $interval->format('%H hours, %I minutes, %S seconds'); 
+                        echo $durasi;
+        */
 
         $this->form_validation->set_rules('tanggal_aktivitas', 'Tanggal', 'required');
         $this->form_validation->set_rules('waktu_mulai', 'Waktu', 'required');
@@ -80,19 +94,27 @@ class TimeTracker extends CI_Controller
                 $hasil['error_selesai'] = "Waktu selesai harus diisi lebih dari waktu mulai";
                 echo json_encode($hasil);
             } else {
-                if ($status == "SELESAI") {
-                    $tanggal_selesai_tugas = date('Y-m-d');
+                //melakukan cek aktivitas agar jam yang sama di tanggal yang sama tidak terinput dua kali
+                $cekWaktuAktivitas = $this->db->query("SELECT * FROM aktivitas WHERE id_tugas_project = '$id_tugas_project' AND tanggal_aktivitas = '$tanggal_aktivitas' AND waktu_mulai = '$waktu_mulai' AND waktu_selesai = '$waktu_selesai'")->num_rows();
+                if ($cekWaktuAktivitas == 1) {
+                    $hasil['sukses'] = false;
+                    $hasil['error_message'] = "Update aktivitas tugas dengan tanggal dan waktu yang diinput sudah terupdate";
+                    echo json_encode($hasil);
                 } else {
-                    $tanggal_selesai_tugas = NULL;
+                    if ($status == "SELESAI") {
+                        $tanggal_selesai_tugas = date('Y-m-d');
+                    } else {
+                        $tanggal_selesai_tugas = NULL;
+                    }
+                    $data = array(
+                        'id_project' => $id_project, 'id_karyawan' => $this->session->userdata('id_karyawan'), 'id_tugas_project' => $id_tugas_project, 'tanggal_aktivitas' => $tanggal_aktivitas,
+                        'waktu_mulai' => $waktu_mulai, 'waktu_selesai' => $waktu_selesai, 'bukti' => $bukti, 'durasi' => $durasi, 'hari' => $hari
+                    );
+                    $this->M_TimeTracker->insert_record($data, 'aktivitas');
+                    $this->M_TimeTracker->update_record(['id_tugas_project' => $id_tugas_project], ['status_tugas' => $status, 'tanggal_selesai_tugas' => $tanggal_selesai_tugas], 'tugas_project');
+                    $hasil['sukses'] = "Berhasil upload aktivitas";
+                    echo json_encode($hasil);
                 }
-                $data = array(
-                    'id_project' => $id_project, 'id_karyawan' => $this->session->userdata('id_karyawan'), 'id_tugas_project' => $id_tugas_project, 'tanggal_aktivitas' => $tanggal_aktivitas,
-                    'waktu_mulai' => $waktu_mulai, 'waktu_selesai' => $waktu_selesai, 'bukti' => $bukti
-                );
-                $this->M_TimeTracker->insert_record($data, 'aktivitas');
-                $this->M_TimeTracker->update_record(['id_tugas_project' => $id_tugas_project], ['status_tugas' => $status, 'tanggal_selesai_tugas' => $tanggal_selesai_tugas], 'tugas_project');
-                $hasil['sukses'] = "Berhasil upload aktivitas";
-                echo json_encode($hasil);
             }
         }
     }
