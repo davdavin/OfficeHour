aa38f43ac88fb9374d9cac9848b70061fc3da00e
happypeoplee <raidavr7@gmail.com>
2022-04-06 20:51:31 +0700

    update

--- application/views/v_detail_project.php
+++ application/views/v_detail_project.php
@@ -326,9 +326,9 @@
                                         <a type="button" class="btn btn-primary" href="<?php echo base_url() . 'ProjectManage/asign_anggota_project_baru/' . $idProject; ?>"><i class="fas fa-plus"></i> Tambah Anggota Project</a><br><br>
                                         <a type="button" class="btn btn-primary" href="<?php echo base_url() . 'ProjectManage/tambah_task_baru/' . $idProject; ?>"><i class="fas fa-plus"></i> Tambah Tugas</a><br><br>
                                         <a type="button" class="btn bg-red batal" href="<?php echo base_url() . 'ProjectManage/konfirmasi_project_tidakselesai/' . $idProject; ?>"><i class="fas fa-times"></i> Tutup Project</a><br><br>
+                                        <a type="button" class="btn btn-primary" href="<?php echo base_url() . 'ProjectManage/konfirmasi_project_selesai/' . $idProject; ?>"><i class="fas fa-check"></i> Selesai Project</a><br><br>
                                     <?php } ?>
-                                    <a type="button" class="btn btn-primary" href="<?php echo base_url() . 'ProjectManage/konfirmasi_project_selesai/' . $idProject; ?>"><i class="fas fa-check"></i> Selesai Project</a><br><br>
-                                    <?php if ($list->status_project == "SELESAI" || $list->status_project == "TIDAK SELESAI") { ?>
+                                    <?php if ($list->status_project == "TIDAK SELESAI") { ?>
                                         <a type="button" class="btn btn-primary lanjut" href="<?php echo base_url() . 'ProjectManage/konfirmasi_project_dilanjutkan/' . $idProject; ?>"><i class="fas fa-check"></i> Melanjutkan Project</a><br><br>
                                 <?php }
                                 } ?>
